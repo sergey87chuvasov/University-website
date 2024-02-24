61b5dd1ebc2b0816a4bb119f3107fd7099d6fcfd
Sergey87 <serge.techno@gmail.com>
2024-02-24 14:58:11 +0300

    added navbar section

--- src/Components/Navbar/Navbar.jsx
+++ src/Components/Navbar/Navbar.jsx
@@ -4,15 +4,17 @@ import logo from '../../assets/logo.png';
 
 function Navbar() {
   return (
-    <nav>
-      <img src={logo} alt='logo pic' />
+    <nav className='container'>
+      <img className='logo' src={logo} alt='logo pic' />
       <ul>
         <li>Home</li>
         <li>Program</li>
         <li>About Us</li>
         <li>Campus</li>
         <li>Testimonials</li>
-        <li>Contact Us</li>
+        <li>
+          <button className='btn'>Contact Us</button>
+        </li>
       </ul>
     </nav>
   );
