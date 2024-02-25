7853ebdd933547761a9d86f66f6c5a7005cbb1a3
Sergey87 <serge.techno@gmail.com>
2024-02-25 18:59:10 +0300

    added programs section

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,15 @@
 import Navbar from './Components/Navbar/Navbar';
 import Hero from './Components/Hero/Hero';
+import Programs from './Components/Programs/Programs';
 
 function App() {
   return (
     <div>
       <Navbar />
       <Hero />
+      <div className='container'>
+        <Programs />
+      </div>
     </div>
   );
 }
