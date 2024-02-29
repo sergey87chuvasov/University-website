9a6a0bbe1eaea9bf314d484c62063dccef7f4739
Sergey87 <serge.techno@gmail.com>
2024-02-29 22:23:52 +0300

    added footer component and react-scroll in project

--- src/App.jsx
+++ src/App.jsx
@@ -6,6 +6,7 @@ import About from './Components/About/About';
 import Campus from './Components/Campus/Campus';
 import Testimonials from './Components/Testimonials/Testimonials';
 import Contact from './Components/Contact/Contact';
+import Footer from './Components/Footer/Footer';
 
 function App() {
   return (
@@ -22,6 +23,7 @@ function App() {
         <Testimonials />
         <Title subtitle='Contact Us' title='Get in Touch' />
         <Contact />
+        <Footer />
       </div>
     </div>
   );
--- src/Components/Navbar/Navbar.jsx
+++ src/Components/Navbar/Navbar.jsx
@@ -1,3 +1,12 @@
+import {
+  Link,
+  Button,
+  Element,
+  Events,
+  animateScroll as scroll,
+  scrollSpy,
+} from 'react-scroll';
+
 import './Navbar.css';
 
 import logo from '../../assets/logo.png';
@@ -15,13 +24,41 @@ function Navbar() {
     <nav className={`container ${sticky ? 'dark-nav' : ''}`}>
       <img className='logo' src={logo} alt='logo pic' />
       <ul>
-        <li>Home</li>
-        <li>Program</li>
-        <li>About Us</li>
-        <li>Campus</li>
-        <li>Testimonials</li>
         <li>
-          <button className='btn'>Contact Us</button>
+          <Link to='hero' smooth={true} offset={0} duration={500}>
+            Home
+          </Link>
+        </li>
+        <li>
+          <Link to='program' smooth={true} offset={-260} duration={500}>
+            Program
+          </Link>
+        </li>
+        <li>
+          <Link to='about' smooth={true} offset={-150} duration={500}>
+            About Us
+          </Link>
+        </li>
+        <li>
+          <Link to='campus' smooth={true} offset={-260} duration={500}>
+            Campus
+          </Link>
+        </li>
+        <li>
+          <Link to='testimonials' smooth={true} offset={-260} duration={500}>
+            Testimonials
+          </Link>
+        </li>
+        <li>
+          <Link
+            to='contact'
+            smooth={true}
+            offset={-260}
+            duration={500}
+            className='btn'
+          >
+            Contact Us
+          </Link>
         </li>
       </ul>
     </nav>
